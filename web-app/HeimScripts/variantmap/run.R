43147e84cbb6790c5fb0544fe57d5b81f60c7e9f
Sascha Herzinger <sascha.herzinger@uni.lu>
2017-01-30 10:21:03 +0100

    annotation script fully functional

--- web-app/HeimScripts/variantmap/run.R
+++ web-app/HeimScripts/variantmap/run.R
@@ -4,14 +4,14 @@ library(TxDb.Hsapiens.UCSC.hg19.knownGene)
 library(BSgenome.Hsapiens.UCSC.hg19)
 
 txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene
-vcfFile <- "/tmp/variants.vcf"
+vcfFile <- "variants.vcf"
 
 main <- function(variants) {
     output <- list()
 
-    save(variants, file="/tmp/variants.Rda")
-    save(loaded_variables, file="/tmp/loaded_variables.Rda")
-    save(fetch_params, file="/tmp/fetch_params.Rda")
+    # save(variants, file="/tmp/variants.Rda")
+    # save(loaded_variables, file="/tmp/loaded_variables.Rda")
+    # save(fetch_params, file="/tmp/fetch_params.Rda")
 
     vcf <- variants
     names(vcf) <- c("subject", "gene", "POS", "REF", "ALT", "CHROM", "var", "frq", "subset")
@@ -21,6 +21,8 @@ main <- function(variants) {
                       INFO=rep(".", nrow(vcf)),
                       FORMAT=rep("GT", nrow(vcf)))
     vcf <- dcast(vcf, CHROM+POS+ID+REF+ALT+QUAL+FILTER+INFO+FORMAT~subject, fill="0|0", value.var="var")
+    annotation.data <- vcf
+    annotation.data <- cbind(QUERYID=rownames(annotation.data), annotation.data)
     NAMES <- names(vcf)
     NAMES[1] <- paste0("#", NAMES[1])
     cat('##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">\n', file=vcfFile)
@@ -28,9 +30,16 @@ main <- function(variants) {
     vcf <- readVcf(vcfFile)
     seqlevels(vcf) = paste0("chr", seqlevels(vcf))
     rd <- rowRanges(vcf)
-    loc <- locateVariants(rd, txdb, CodingVariants())
+    loc <- locateVariants(rd, txdb, AllVariants())
     coding <- predictCoding(vcf, txdb, seqSource=Hsapiens)
-
+    locations <- aggregate(LOCATION ~ QUERYID, data=mcols(loc), FUN=function(x) toString(unique(x)))
+    consequences <- aggregate(CONSEQUENCE ~ QUERYID, data=mcols(coding), FUN=function(x) toString(unique(x)))
+    annotation.data <- merge(consequences, annotation.data, by="QUERYID", all.y=T)
+    annotation.data <- merge(locations, annotation.data, by="QUERYID", all.y=T)
+    annotation.data <- melt(annotation.data, id.vars=c("QUERYID", "LOCATION", "CONSEQUENCE", "CHROM", "POS", "ID", "REF", "ALT", "QUAL", "FILTER", "INFO", "FORMAT"),
+         variable.name="subject", value.name="var")
+    annotation.data <- annotation.data[, names(annotation.data) %in% c("LOCATION", "CONSEQUENCE", "CHROM", "POS", "subject")]
+    names(annotation.data) <- c("location", "consequence", "chr", "pos", "subject")
     if (exists("loaded_variables")) {
         hdd.idx <- grep("^highDimensional", names(loaded_variables))
         cat.idx <- grep("^categoric", names(loaded_variables))
@@ -44,12 +53,13 @@ main <- function(variants) {
         })
         hdd.data <- do.call(rbind, hdd.data)
         names(hdd.data) <- c("Row.Label", "gene", "subject", "expr")
-        data <- merge(variants, hdd.data[, -1], by=c("subject", "gene"), all.x=T)
-        output$data <- data
+        expression.data <- merge(variants, hdd.data[, -1], by=c("subject", "gene"), all.x=T)
+        data <- merge(annotation.data, expression.data, by=c("subject", "chr", "pos"))
     } else {
-        output$data <- variants
+        data <- annotation.data
     }
 
+    output$data <- data
     json <- toJSON(output)
     return(json)
 }
