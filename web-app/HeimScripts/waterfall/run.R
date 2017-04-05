25206b95376ad2f6072c662e3297a4524d0377bf
Sorsby <sorsby@gmail.com>
2017-04-05 15:20:09 +0100

    Bug fix

--- web-app/HeimScripts/waterfall/run.R
+++ web-app/HeimScripts/waterfall/run.R
@@ -2,7 +2,7 @@
 main <- function(lowRangeOperator = "&lt;", lowRangeValue = "", highRangeOperator = "&lt;", highRangeValue = "") {
 
     save(loaded_variables, file="/tmp/data/loaded_variables.Rda")
-    save(fetch_params$ontologyTerms, file="/tmp/data/fetch_params.Rda")
+    save(fetch_params, file="/tmp/data/fetch_params.Rda")
 
     stop(paste("R Script: lowRangeOperator = '", lowRangeOperator, "'  lowRangeValue = '", lowRangeValue, "'  highRangeOperator = '", highRangeOperator, "'  highRangeValue = '", highRangeValue, "'"), sep="")
 
