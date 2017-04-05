67cf5d3ce9144ec780a1594f076b95f95e466749
Sorsby <sorsby@gmail.com>
2017-04-05 12:08:51 +0100

    Bugs fix

--- web-app/HeimScripts/waterfall/run.R
+++ web-app/HeimScripts/waterfall/run.R
@@ -9,7 +9,7 @@ main <- function(lowRangeOperator = "&lt;", lowRangeValue = "", highRangeOperato
 
     #Read the input data.
     #if (nrow(num_data) == 0) {
-#    stop(paste("Variable '", fetch_params$ontologyTerms$num_data_n0$name, "' has no patients for subset 1"), sep="")
+    stop("R Script Stops here...")
 #}
 }
 
